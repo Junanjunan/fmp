7de43fbb7284276fe8a70528bcb7221123021be0
Junanjunan <taltalrealty@gmail.com>
2024-12-06 02:11:51 +0900

    StockService: [fix] Remove unnecessary try-except clause

--- services/stocks.py
+++ services/stocks.py
@@ -13,38 +13,34 @@ class StockService:
         self.request = RequestFMP()
 
     def renew_stocks_list(self):
-        try:
-            url = ApiUrlEnum.STOCK_LIST.value.url
-            api_result = self.request.get_api_result(url)
-            updated_at = datetime.datetime.now()
+        url = ApiUrlEnum.STOCK_LIST.value.url
+        api_result = self.request.get_api_result(url)
+        updated_at = datetime.datetime.now()
 
-            with Database() as db:
-                symbols_in_db = db.get_symbols()
-                for stock_data in api_result:
-                    symbol = stock_data['symbol']
-                    price = stock_data['price']
-                    name = stock_data['name'] if stock_data['name'] else 'None'
-                    type_id = stock_data['type']
-                    exchange_id = stock_data['exchangeShortName']
-                    if symbol in symbols_in_db:
-                        print('update',stock_data)
-                        symbols_in_db[symbol] = True
-                        db.update_symbol_normal_data(
-                            symbol, name, price, type_id, exchange_id, updated_at
-                        )
-                    else:
-                        print('insert',stock_data)
-                        db.insert_symbol_normal_data(
-                            symbol, name, price, type_id, exchange_id, updated_at
-                        )
-                disappeared_symbols = [symbol for symbol, is_existing in symbols_in_db.items() if not is_existing]
-                for symbol in disappeared_symbols:
-                    db.update_symbol_disappeared_data(symbol, updated_at)
-                db.conn.commit()
-            print(f"Updated {len(api_result)} stocks")
-        except Exception as e:
-            db.conn.rollback()
-            print(f"Error: {e}")
+        with Database() as db:
+            symbols_in_db = db.get_symbols()
+            for stock_data in api_result:
+                symbol = stock_data['symbol']
+                price = stock_data['price']
+                name = stock_data['name'] if stock_data['name'] else 'None'
+                type_id = stock_data['type']
+                exchange_id = stock_data['exchangeShortName']
+                if symbol in symbols_in_db:
+                    print('update',stock_data)
+                    symbols_in_db[symbol] = True
+                    db.update_symbol_normal_data(
+                        symbol, name, price, type_id, exchange_id, updated_at
+                    )
+                else:
+                    print('insert',stock_data)
+                    db.insert_symbol_normal_data(
+                        symbol, name, price, type_id, exchange_id, updated_at
+                    )
+            disappeared_symbols = [symbol for symbol, is_existing in symbols_in_db.items() if not is_existing]
+            for symbol in disappeared_symbols:
+                db.update_symbol_disappeared_data(symbol, updated_at)
+            db.conn.commit()
+        print(f"Updated {len(api_result)} stocks")
 
 
 if __name__ == "__main__":
