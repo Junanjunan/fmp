d877d9392061cc6724def8f85ab719cd00c9be87
Junanjunan <taltalrealty@gmail.com>
2024-12-06 00:27:59 +0900

    Database: [add] Add columns to symbols table (is_existing, updated_at)

--- database/sql/reals.sql
+++ database/sql/reals.sql
@@ -12,6 +12,8 @@ CREATE TABLE symbols (
     name VARCHAR(50) NOT NULL,
     type_id VARCHAR(20),
     exchange_id VARCHAR(20),
+    is_existing BOOLEAN DEFAULT TRUE,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (type_id) REFERENCES types(id),
     FOREIGN KEY (exchange_id) REFERENCES exchanges(id)
 );
