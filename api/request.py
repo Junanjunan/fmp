aa9cf8c9980878ec28d85babe6d2a2e6b93e14a9
Junanjunan <taltalrealty@gmail.com>
2024-12-08 21:29:49 +0900

    RequestFMP: [fix] Pass params argument to get_api_url params for flexibility

--- api/request.py
+++ api/request.py
@@ -16,10 +16,15 @@ class RequestFMP:
         self.active_api_keys.remove(inactive_api_key)
         self.inactive_api_keys.append(inactive_api_key)
 
-    def get_api_url(self, url):
-        return f"{url}?apikey={self.api_key}"
+    def get_api_url(self, url, params):
+        """params is a dictionary of parameters to add to the URL"""
+        if params:
+            queries = '&'.join([f"{key}={value}" for key, value in params.items()])
+            return f"{url}?{queries}&apikey={self.api_key}"
+        else:
+            return f"{url}?apikey={self.api_key}"
 
-    def get_api_result(self, url):
-        api_url = self.get_api_url(url)
+    def get_api_result(self, url, params=None):
+        api_url = self.get_api_url(url, params)
         response = requests.get(api_url)
         return response.json()
